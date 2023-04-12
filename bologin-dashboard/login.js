2e7d5cdf8763281d6eff2e4bed01ac75a3a23a87
Joaquin de Boer <joaquindeboer@Joaquins-MacBook-Pro.local>
2023-04-12 19:52:48 +0200

    code

---
+++ bologin-dashboard/login.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('#login-form');
+const errorMessage = document.querySelector('#error-message');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = form.elements['email'].value;
+    const password = form.elements['password'].value;
+
+    if (email !== 'voorbeeld@bo.com' || password !== 'password') {
+        errorMessage.textContent = 'Invalid email or password.';
+    } else {
+        errorMessage.textContent = '';
+        window.location.href = 'dashboard.html';
+    }
+});
