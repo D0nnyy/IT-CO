e0803a4d5d82782304ddd94dfef6e75e88c1c767
Joaquin de Boer <joaquindeboer@Joaquins-MacBook-Pro.local>
2023-04-12 21:54:04 +0200

    arduino licht

--- bologin-dashboard/dashboardf/script.js
+++ bologin-dashboard/dashboardf/script.js
@@ -1,34 +1,44 @@
 const apiKey = '3803aa760d1beaccfbe87ec6f1e141fe';
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${apiKey}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${apiKey}`;
 
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=3803aa760d1beaccfbe87ec6f1e141fe&mode=json&units=metric&lang=nl")
+fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    // update de temperatuur
-    document.getElementById('temperature').textContent = `${data.main.temp}°C`;
-    document.getElementById('description').textContent = data.weather[0].description;
+    // update the temperature
+    const tempEl = document.querySelector('.temp');
+    tempEl.textContent = `${data.main.temp}°C`;
+
+    // update the description
+    const descEl = document.querySelector('.description');
+    descEl.textContent = data.weather[0].description;
+
+    // update the min and max temperatures
+    const minTempEl = document.querySelector('.min-temp');
+    minTempEl.textContent = `${data.main.temp_min}°C`;
+    const maxTempEl = document.querySelector('.max-temp');
+    maxTempEl.textContent = `${data.main.temp_max}°C`;
+
+    // update the humidity
+    const humidityEl = document.querySelector('.humidity');
+    humidityEl.textContent = `${data.main.humidity}%`;
+
+    // update the pressure
+    const pressureEl = document.querySelector('.pressure');
+    pressureEl.textContent = `${data.main.pressure} hPa`;
+
+    // update the wind speed and direction
+    const windSpeedEl = document.querySelector('.wind-speed');
+    windSpeedEl.textContent = `${data.wind.speed} m/s`;
+    const windDirectionEl = document.querySelector('.wind-direction');
+    windDirectionEl.textContent = `${data.wind.deg}°`;
+
+    // update the sunrise and sunset times
+    const sunriseEl = document.querySelector('.sunrise');
+    sunriseEl.textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+    const sunsetEl = document.querySelector('.sunset');
+    sunsetEl.textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+  })
+  
+  .catch(error => {
+    console.log(`An error occurred while fetching the data: ${error}`);
   });
-
-// update the clock every second
-setInterval(() => {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  // add leading zeros to minutes and seconds
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-
-  // update the clock
-  document.getElementById('clock').textContent = `${hours}:${formattedMinutes}:${formattedSeconds}`;
-}, 1000);
-
-
-const tempEl = document.querySelector('.temp');
-const descEl = document.querySelector('.description');
-const minTempEl = document.querySelector('.min-temp');
-const maxTempEl = document.querySelector('.max-temp');
-const humidityEl = document.querySelector('.humidity');
-const pressureEl = document.querySelector('.pressure');
